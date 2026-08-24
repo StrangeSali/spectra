1c1e7f21cdccd5c8b2f2190b596bdae4292d73e1
Ana Chaves <apechaves17@gmail.com>
2026-08-24 17:07:28 +0200

    test: dummy classifier added temporarily

--- spectra/processing/audio_processing.py
+++ spectra/processing/audio_processing.py
@@ -1,8 +1,7 @@
 import sys
-import queue
-import threading
 import numpy as np
 import pyaudio
+from sklearn.dummy import DummyClassifier
 
 # --- YAMNET COMPATIBLE CONFIGURATION ---
 SAMPLE_RATE = 16000
@@ -11,14 +10,23 @@ MAX_QUEUE_SIZE = 10
 CHANNELS = 1
 FORMAT = pyaudio.paFloat32
 
-# The thread-safe Bridge Queue for asynchronous streaming
-audio_queue = queue.Queue(maxsize=MAX_QUEUE_SIZE)
 
-def sound_to_sample():
+def create_model():
+
+    classy = DummyClassifier(strategy="most_frequent")
+    X_train = np.zeros((10, CHUNK_SIZE)) # 10 dummy samples
+    y_train = np.array([0, 1, 2, 1, 0, 2, 0, 1, 2, 1]) # Fixed array format
+    classy.fit(X_train, y_train)
+
+    return classy
+
+
+def sound_to_sample(model):
     """
     Worker Thread 1: Handles the microphone hardware stream.
     Captures chunks, scales them, and drops them into the queue.
     """
+
     pa = pyaudio.PyAudio()
 
     mic = pa.open(
@@ -28,32 +36,21 @@ def sound_to_sample():
         input=True,
         frames_per_buffer=CHUNK_SIZE)
 
+    print("Listening...Press CTRL+C to kill it")
+
     while True:
 
         # 1. Grab raw audio bytes from hardware
         data = mic.read(CHUNK_SIZE, exception_on_overflow=False)
 
         # 2. Convert to perfectly scaled float32 NumPy array (-1.0 to 1.0)
-        samples = np.frombuffer(data, dtype=np.float32)
 
-        # 3. Asynchronously push to queue (Drop oldest if YAMNet falls behind)
-        try:
-            audio_queue.put_nowait(samples)
-        except queue.Full:
-            try:
-                audio_queue.get_nowait()
-                audio_queue.put_nowait(samples)
-            except queue.Empty:
-                pass
+        sample = np.frombuffer(data, dtype=np.float32)
+
+        y_pred = model.predict(sample)
+
+        print(y_pred)
 
 if __name__ == "__main__":
-    # Start the microphone collector in the background (Thread 1)
-    mic_worker = threading.Thread(target=sound_to_array_thread, daemon=True)
-    mic_worker.start()
-
-    # Run the YAMNet consumer loop on the main thread (Thread 2)
-    try:
-        yamnet_inference_thread()
-    except KeyboardInterrupt:
-        print("\nProgram stopped by user. Clean exit completed.")
-        sys.exit(0)
+    model = create_model()
+    sound_to_sample(model)
